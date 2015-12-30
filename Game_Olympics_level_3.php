a77f10d3e12ad1703edac494756d76312b5969b9
ryan dowler <Ryandowler9@hotmail.com>
2015-12-30 15:38:42 +0000

    started coding snake level

---
+++ Game_Olympics_level_3.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Level 3: Snake</title>
+        <!--requires here ryan -->
+        <style>
+        </style>
+        <script>
+            //constants
+            var COlS = 26, ROWS = 26;
+            var EMPTY = 0, SNAKE = 1, FOOD = 2;
+            VAR grid = {
+
+            width = null;
+            height = null;
+            _grid = null;
+            
+                //---FUNCTIONS---//
+            
+                //direction, columns, rows 
+                init: function(d, c, r){
+
+
+                },
+                // val, x position, y position
+                set: function(val, x, y){
+
+                },
+                
+                get: function(x,y){
+                    
+                }
+            }
+            
+            //----------SNAKE----------//
+
+
+        </script>
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
