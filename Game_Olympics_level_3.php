5c9e6fe63567672337cce877936b5dfce8e0524d
ryan dowler <Ryandowler9@hotmail.com>
2015-12-30 20:18:58 +0000

    snake can move, but does not grow when eats food, food does not respawn when eaten either!!

--- Game_Olympics_level_3.php
+++ Game_Olympics_level_3.php
@@ -34,7 +34,6 @@
                     KEY_UP = 38,
                     KEY_RIGHT = 39,
                     KEY_DOWN = 40,
-                   
                     //Game Objects
                     canvas, /* HTMLCanvas */
                     ctx, /* CanvasRenderingContext2d */
@@ -42,18 +41,17 @@
                     frames, /* number, used for animation */
                     score;	  /* number, keep track of the player score */
 
-             // -------- GRID -------- //
+            // -------- GRID -------- //
             grid = {
-                width: null, 
+                width: null,
                 height: null,
-                _grid: null, 
-
+                _grid: null,
                 init: function (d, c, r) {
                     this.width = c;
                     this.height = r;
 
                     this._grid = [];
-                    
+
                     for (var x = 0; x < c; x++) {
                         //push empty array
                         this._grid.push([]);
@@ -63,11 +61,10 @@
                         }
                     }
                 },
-                 // val, x position, y position
+                // val, x position, y position
                 set: function (val, x, y) {
                     this._grid[x][y] = val;
                 },
-              
                 //Get the value of the cell at (x, y)
                 get: function (x, y) {
                     // return the value at the cell
@@ -82,9 +79,9 @@
                  the queue */
                 _queue: null, /* Array<number>, data representation*/
 
-               //---FUNCTIONS---//
-              
-              
+                //---FUNCTIONS---//
+
+
                 //direction, x position, y position
                 init: function (d, x, y) {
                     this.direction = d;
@@ -92,14 +89,12 @@
                     this._queue = [];
                     this.insert(x, y);
                 },
-                
                 //x position, y position
                 insert: function (x, y) {
                     // unshift prepends an element to an array
                     this._queue.unshift({x: x, y: y});
                     this.last = this._queue[0];
                 },
-                
                 //Removes and returns the first element in the queue
                 remove: function () {
                     //  return the first element
@@ -107,7 +102,7 @@
                 }
             };
 
-            
+
             //Set a food id at a random free cell in the grid 
             function setFood() {
                 var empty = [];
@@ -125,8 +120,8 @@
             }
 
             // ------ GAME FUNTIONS ------//
-            
-            
+
+
             //Starts the game
             function main() {
                 // create and initiate the canvas element
@@ -137,18 +132,24 @@
                 // add the canvas element to the body of the document instead of the long way(html and refer to it)
                 document.body.appendChild(canvas);
 
-               
-               
+
+
 
                 frames = 0;
                 keystate = {};
-              
+                document.addEventListener("keydown", function(evt){
+                    keystate[evt.keyCode] = true;
+                });
+                document.addEventListener("keyup", function(evt){
+                    delete keystate[evt.keyCode];
+                });
+
                 // intatiate game objects and starts the game loop
                 init();
                 loop();
             }
 
-            
+
             //Resets and inits game objects
             function init() {
                 score = 0;
@@ -162,13 +163,13 @@
                 setFood();
             }
 
-            
+
             //The game loop function, used for game updates and rendering
-             
+
             function loop() {
                 update();
                 draw();
-                
+
                 window.requestAnimationFrame(loop, canvas);
             }
 
@@ -178,13 +179,18 @@
             function update() {
                 frames++;
                 
+                if(keystate[KEY_LEFT]) snake.direction = LEFT;
+                if(keystate[KEY_UP]) snake.direction = UP;
+                if(keystate[KEY_RIGHT]) snake.direction = RIGHT;
+                if(keystate[KEY_DOWN]) snake.direction = DOWN;
+
                 //every 5 frames
-                if(frames%5 === 0){
+                if (frames % 5 === 0) {
                     var nx = snake.last.x;
                     var ny = snake.last.y;
-                    
-                    switch(snake.direction){
-                        case LEFT: 
+
+                    switch (snake.direction) {
+                        case LEFT:
                             nx--;
                             break;
                         case UP:
@@ -195,29 +201,29 @@
                             break;
                         case DOWN:
                             ny++;
-                            break;  
+                            break;
                     }
-                    
+
                     var tail = snake.remove();
                     grid.set(EMPTY, tail.x, tail.y);
                     tail.x = nx;
                     tail.y = ny;
                     grid.set(SNAKE, tail.x, tail.y);
-                    
+
                     snake.insert(tail.x, tail.y);
-                    
+
                     //if snake hits any walls it dies and restart by calling init()
-                    if(0> nx || nx >  grid.width-1 ||
-                       0> ny || ny > grid.height-1){
-                   
+                    if (0 > nx || nx > grid.width - 1 ||
+                            0 > ny || ny > grid.height - 1) {
+
                         return init();
                     }
-                    
+
                 }
 
-                
-                }
-            
+
+            }
+
 
             /**
              * Render the grid to the canvas.
@@ -226,12 +232,12 @@
                 // calculate tile-width and -height
                 var tw = canvas.width / grid.width;
                 var th = canvas.height / grid.height;
-                
-                
+
+
                 // iterate through the grid and draw all cells
                 for (var x = 0; x < grid.width; x++) {
                     for (var y = 0; y < grid.height; y++) {
-                        
+
                         // each cell
                         switch (grid.get(x, y)) {
                             case EMPTY:
